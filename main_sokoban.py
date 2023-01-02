1584f209d8f862dd99cdc364cf667569d0c80183
tomyaacov <tomyaacov1210@gmail.com>
2023-01-02 10:30:42 +0200

    pickle sokoban results

--- main_sokoban.py
+++ main_sokoban.py
@@ -5,6 +5,11 @@ from algorithms.value_iteration import ValueIteration
 
 from examples.sokoban_pygame.sokoban_maps import maps
 import sys
+import pickle
+
+def save_obj(obj, file_name):
+    with open(file_name, 'wb') as f:
+        pickle.dump(obj, f)
 
 eval_runs = 1000
 eval_run_max_length = 1000
@@ -12,14 +17,17 @@ eval_run_max_length = 1000
 if len(sys.argv) > 1:
     i = int(sys.argv[1])
 else:
-    eval_runs = 10
-    eval_run_max_length = 10
-    i = 0
+    eval_runs = 5
+    eval_run_max_length = 1000
+    i = 1
 map_settings["map"] = maps[i]
 pygame_settings["display"] = False
 dfs = DFSBProgram(init_bprogram)
 (init, states_dict, events, liveness_bthreads), dfs_time = dfs.run()
+print("dfs_time:", dfs_time)
+save_obj(states_dict, "output/states_dict_" + str(i))
 states = list(states_dict)
+save_obj(states, "output/states_" + str(i))
 print("graph size:", len(states))
 print("graph edges:", sum([len(s.transitions) for s in states]))
 graph = DFSBProgram.save_graph(init, states, "output/graph_sokoban_" + str(i) + ".dot")
@@ -28,6 +36,7 @@ spot_ess, spot_time = SpotSolver.compute_ess(states_dict, events, liveness_bthre
 #spot_success_rate = SpotSolver.evaluate(spot_ess, init_bprogram, eval_runs, eval_run_max_length)
 print("spot_time:", spot_time)
 #print("spot_success_rate:", spot_success_rate)
+save_obj(spot_ess, "output/spot_ess_" + str(i))
 
 
 value_iteration_ess, value_iteration_time = ValueIteration.compute_ess(states, 0.99, 0.01, liveness_bthreads)
@@ -35,7 +44,7 @@ value_iteration_ess.spot_ess = spot_ess
 value_iteration_ess.spot_ess.reset_to_initial()
 value_iteration_success_rate = ValueIteration.evaluate(value_iteration_ess, init_bprogram, eval_runs,
                                                         eval_run_max_length)
-
+save_obj(value_iteration_ess, "output/value_iteration_ess_" + str(i))
 print("value_iteration_time:", value_iteration_time)
 print("value_iteration_success_rate:", value_iteration_success_rate)
 
